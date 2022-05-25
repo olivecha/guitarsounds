54a123fc692c6c5079d7cb4e67111660123a2e04
olivecha <olivier.chabot.2@ens.etsmtl.ca>
2022-05-24 23:57:44 -0400

    verify GitHub action test

---
+++ tests/test_Sound.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        pass
+        #self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
