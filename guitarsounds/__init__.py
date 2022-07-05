2b2bb059d5b375b93aaf7fea15f52c34b8751009
olivecha <olivier.chabot.2@ens.etsmtl.ca>
2022-07-05 11:39:01 -0400

    fixed module init

--- guitarsounds/__init__.py
+++ guitarsounds/__init__.py
@@ -2,6 +2,7 @@
 from guitarsounds.interface import guitarGUI as Interface
 from guitarsounds.analysis import Signal, Sound, SoundPack, Plot
 from guitarsounds.analysis import plt
+import guitarsounds.helpers_tests
 show = plt.show
 
 # Version of the guitarsounds package
